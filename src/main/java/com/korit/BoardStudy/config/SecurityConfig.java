4707b12db34ff0e426a5850d1669b5ec1b6e15a0
hdni2002 <hdni2003@gmail.com>
2025-07-03 20:46:32 +0900

    Feat:Commit

--- src/main/java/com/korit/BoardStudy/config/SecurityConfig.java
+++ src/main/java/com/korit/BoardStudy/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.korit.BoardStudy.config;
 
 
+import com.korit.BoardStudy.security.filter.JwtAuthenticaionFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,12 +10,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticaionFilter jwtAuthenticaionFilter;
+
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
@@ -43,6 +50,7 @@ public class SecurityConfig {
 
         http.sessionManagement(Session -> Session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+        http.addFilterBefore(jwtAuthenticaionFilter, UsernamePasswordAuthenticationFilter.class);
         http.authorizeHttpRequests(auth -> {
             auth.requestMatchers("").permitAll();
             auth.anyRequest().authenticated();
